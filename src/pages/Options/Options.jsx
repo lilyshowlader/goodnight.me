8cbab70ecf33b85e50c91c23148f38347519451d
lilyshowlader <lilyshowlader@gmail.com>
2022-11-25 00:25:49 -0500

    update options page css

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom"
-
+import styles from "./Options.module.css"
 const Options = () => {
   return (
     <>
-    <p>what would you like to do?</p>
-    <p><Link to='/useapp'>how to use this app</Link></p>
-    <p><Link to='/viewlogs'>view my sleep logs</Link></p>
-    <p><Link to='/logfortoday'>log for today</Link></p>
+    <p className={styles.fontOne}>what would you <br></br> like to do?</p>
+    <div className={styles.buttonContainer}>
+    <p><Link to='/useapp' className={styles.fontTwo}>how to use this app</Link></p>
+    </div>
+    <div className={styles.buttonContainer}>
+    <p><Link to='/viewlogs className={styles.fontTwo}'className={styles.fontTwo}>view my sleep logs</Link></p>
+    </div>
+    <p><Link to='/logfortoday'className={styles.fontTwo}>log for today</Link></p>
     </>
   )
 }
