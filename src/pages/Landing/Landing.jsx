f873b4c4eea5afb2a0bf8ed376477eaaeabf7880
lilyshowlader <lilyshowlader@gmail.com>
2022-11-25 15:44:25 -0500

    update css useapp page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className={styles.container}>
     <p className={styles.logoFont}>goodnight.me</p>
     <img className={styles.img}src="https://i.imgur.com/s0WcVLi.png" alt="moon"/>
-    <p>click <Link to='/Options'>here </Link><br></br> to continue</p>
+    <p>click <Link to='/Options' className={styles.linkFont}>here </Link><br></br> to continue</p>
     </div>
   )
 }
--- src/pages/UseApp/UseApp.jsx
+++ src/pages/UseApp/UseApp.jsx
@@ -1,7 +1,10 @@
+import styles from "./UseApp.module.css"
+
 const UseApp = () => {
   return (
     <>
-    <p>step one</p>
+    <div className={styles.container}>
+    <p className={styles.stepOne}>step one</p>
     <h1>get cozy</h1>
     <p>step two</p>
     <h1>open this app</h1>
@@ -9,6 +12,7 @@ const UseApp = () => {
     <h1>log your day</h1>
     <p>step four</p>
     <h1>keep track of your sleep patterns</h1>
+    </div>
     </>
   )
 }
